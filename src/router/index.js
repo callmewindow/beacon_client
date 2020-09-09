8ac959f638ff3d4c09a0bb7da051cfa798e3501d
kankanshukashu <1411607509@qq.com>
2020-09-09 23:24:04 +0800

    帖子详情页初具雏形
    Signed-off-by: kankanshukashu <1411607509@qq.com>

--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,7 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
 import Home from '../views/Home.vue'
+import PostDetail from "../views/PostDetail";
 
 Vue.use(VueRouter)
 
@@ -11,13 +12,10 @@ Vue.use(VueRouter)
     component: Home
   },
   {
-    path: '/about',
-    name: 'About',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
-  }
+    path: '/postdetail',
+    name: 'PostDetail',
+    component: PostDetail
+  },
 ]
 
 const router = new VueRouter({
