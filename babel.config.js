16761c5131c0f36b775fff1f69e1bdb482c212e8
callmewindow <847590417@qq.com>
2020-09-11 11:34:20 +0800

    删除可能的跨域问题解决代码

--- babel.config.js
+++ babel.config.js
@@ -2,17 +2,17 @@ module.exports = {
   presets: [
     '@vue/cli-plugin-babel/preset'
   ],
-  devServer: {
-    proxy: {
-      '/': {
-        target: 'http://101.200.219.50', // 对应自己的接口
-        changeOrigin: true,
-        ws: false,
-        pathRewrite: {
-          '^/': '',
-        },
-      },
+  // devServer: {
+  //   proxy: {
+  //     '/': {
+  //       target: 'http://101.200.219.50', // 对应自己的接口
+  //       changeOrigin: true,
+  //       ws: false,
+  //       pathRewrite: {
+  //         '^/': '',
+  //       },
+  //     },
 
-    },
-  },
+  //   },
+  // },
 }
