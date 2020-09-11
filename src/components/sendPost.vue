eccff5b45312f441a92f5ee26247f53399156a79
callmewindow <847590417@qq.com>
2020-09-11 23:05:19 +0800

    新增字体设置，新增帖子发布组件

--- src/components/sendPost.vue
+++ src/components/sendPost.vue
@@ -1,17 +1,45 @@
 <template>
-  <div id="sendPA">
-
-    <el-select
-      v-model="value"
-      multiple
-      filterable
-      allow-create
-      default-first-option
-      placeholder="请选择文章标签"
-    >
-      <el-option v-for="item in options" :key="item.value" :label="item.label" :value="item.value"></el-option>
-    </el-select>
-  </div>
+  <el-card id="sendPA">
+    <div slot="header">
+      发布帖子
+      <el-button slot="button" style="float: right;padding: 3px 0" type="text" @click="cancelSend">取消</el-button>
+    </div>
+    <div class="sendItem">
+      <div class="itemTitle"><span style="color:red">*</span>帖子标题</div>
+      <el-input v-model="post.title" placeholder="请输入内容" maxlength="30" show-word-limit></el-input>
+    </div>
+    <div class="sendItem">
+      <div class="itemTitle"><span style="color:red">*</span>帖子内容</div>
+      <el-input
+        type="textarea"
+        :rows="5"
+        placeholder="请输入内容"
+        v-model="post.content"
+        resize="none"
+        maxlength="500"
+        show-word-limit
+      ></el-input>
+    </div>
+    <div class="sendItem" style="width:50%">
+      <div class="itemTitle">帖子标签</div>
+      <el-select
+        v-model="value"
+        multiple
+        filterable
+        allow-create
+        default-first-option
+        placeholder="添加额外标签"
+      >
+        <el-option
+          v-for="item in options"
+          :key="item.value"
+          :label="item.label"
+          :value="item.value"
+        ></el-option>
+      </el-select>
+    </div>
+    <el-button id="sendBtn" type="primary" size="small" @click="sendPost">发布</el-button>
+  </el-card>
 </template>
 
 <script>
@@ -20,34 +48,50 @@ export default {
   data() {
     return {
       post: {
-        title: "这是帖子标题",
-        content: "这是帖子内容",
+        title: "",
+        content: "",
       },
       options: [
         {
-          value: "",
-          label: "HTML",
+          value: "学习讨论",
+          label: "学习讨论",
         },
         {
-          value: "CSS",
-          label: "CSS",
-        },
-        {
-          value: "JavaScript",
-          label: "JavaScript",
+          value: "课程链接",
+          label: "课程链接",
         },
       ],
       value: [],
     };
   },
+  methods:{
+    cancelSend(){
+      alert("取消")
+    },
+    sendPost(){
+      alert("发送")
+    }
+  }
 };
 </script>
 
 <style scoped>
-#sendPA{
+#sendPA {
   width: 500px;
   height: 600px;
   margin-left: 50px;
   margin-top: 50px;
+}
+.sendItem{
+  width: 80%;
+  margin: 20px 0;
+}
+.sendItem:nth-child(1){
+  margin-top: 0;
+}
+.itemTitle{
+  font-size: 14px;
+  font-weight: bold;
+  margin: 5px 0;
 }
 </style>
