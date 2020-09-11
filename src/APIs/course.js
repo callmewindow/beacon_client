c0a84cf9b0ed75b61fb7a44f0cef22ba06b4e816
Andersen <792005029@qq.com>
2020-09-11 22:43:54 +0800

    暂时注释化未使用的QS导入

--- src/APIs/course.js
+++ src/APIs/course.js
@@ -1,8 +1,8 @@
 import axios from 'axios'
 
-import {
-    QS
-} from './../tools/apiTool'
+// import {
+//     QS
+// } from './../tools/apiTool'
 
 export const getCourseInfo = (courseId) => {
     return axios({
