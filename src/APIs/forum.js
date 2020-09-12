ba8bc5c2494b17c194d267cbc923ea3336cd85cf
callmewindow <847590417@qq.com>
2020-09-12 10:19:54 +0800

    更新部分接口

--- src/APIs/forum.js
+++ src/APIs/forum.js
@@ -1,6 +1,42 @@
 // import axios from 'axios'
 
+import Axios from "axios"
+
 // import {
 //   QS
 // } from './../tools/apiTool'
 
+export const postQuery = (postId) => {
+    return axios({
+        method: 'POST',
+        url: `/postQuery`,
+        data: {
+            id: postId
+        }
+    })
+}
+
+export const createPost = (post) => {
+    return axios({
+        method: 'POST',
+        url: `/createPost`,
+        data: {
+            title: post.title,
+            course: post.courseId,
+            owner: post.senderId,
+            tag: post.value,
+            content: post.content
+        }
+    })
+}
+
+export const setForumRule = (forumId, forumCon) => {
+    return axios({
+        method: 'POST',
+        url: `/setCourseRule`,
+        data: {
+            id: forumId,
+            rule: forumCon
+        }
+    })
+}
